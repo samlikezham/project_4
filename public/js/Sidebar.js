e8628352fcff71f748f0ac249cb76b880adff395
Alexis <alexis.bohlander@gmail.com>
2018-08-13 11:42:51 -0600

    added .ruby-version and .gitignore

---
+++ public/js/Sidebar.js
@@ -0,0 +1,31 @@
+//  #########     CONSTRUCTOR     ###########
+//  Keep track of in state
+//    current score
+//    prompt toggle to show
+
+
+
+
+//  ########  FUNCTIONS    ###########
+//  score tracker
+//    pass in value of question
+//    pass in boolean true for correct false for incorrect
+//    this.setState to update score -> will update page
+
+
+//  prompt toggle
+//    get current value and set to !value
+
+
+
+
+
+
+
+
+
+
+//########     RENDER      ###########
+//show player current score
+//    remove buttons and question and answer
+//  in place put prompt to choose next question on the board
