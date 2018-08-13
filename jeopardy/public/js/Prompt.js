e94e8a17135d3524a7c8574aa4848f6ffbf26b1a
Sam <schung015@gmail.com>
2018-08-13 14:10:50 -0700

    getting everything synced

--- jeopardy/public/js/Prompt.js
+++
@@ -1,4 +0,0 @@
-
-// ############   RENDER     ###############
-
-// prompt to pick another question 
--- public/js/Question.js
+++ public/js/Question.js
@@ -1,25 +1,78 @@
 //  #########     CONSTRUCTOR     ###########
-
-//  question
-//    answer toggle to show
-//  PROPS we expect to receive
-//    question
-//    update scoreboard - function passed in from sidebar
-//    toggle prompt - function passed in from sidebar
-
-//   ###########  FUNCTIONS     ###########
-
-//  answer toggle
-//    get current value and set to !value
+	//  PROPS we expect to receive
+	// question
+class Question extends React.Component {
+	constructor(props){
+		super(props)
+		this.isAnswered = this.isAnswered.bind(this)
+	}
 
 
-//    #########   RENDER    ###############
-//if looking at a question
-//  show question
-//  show button to reveal answer
-//  if click button to reveal answer
-//    then reveal the answer
+	//  toggle prompt - function passed in from sidebar
+	//  update scoreboard - function passed in from sidebar
+
+
+
+	//   ###########  FUNCTIONS     ###########
+	// get current value and set to !value
+	// answer toggle to show
+
+	isAnswered(){
+		
+	}
+
+
+
+//  ##############   RENDER    ###############
+	render(){
+
+		return <div>
+
+			{/* if looking at question - show question */}
+			<div className="questionDisplay">
+				<h1>{this.props.data.question}</h1>
+				<li><strong>Category:</strong> {this.props.data.category.title}</li>
+				<li><strong>Value:</strong> {this.props.data.value} </li>
+			</div>
+
+			{/* btn to reveal answer */}
+			<div className="answer">
+				<button onClick={()=> {this.props.toggleAnswer()}}>Click to Reveal Answer</button>
+			</div>
+
+
+		
+			<button onClick={this.isAnswered}>Correct</button>
+			<button onClick={this.isAnswered}>Incorrect</button>
+			<button onClick={this.isAnswered}>No Answer</button>
+			{/* toggle score btns after */}
+			<div>
+				
+			</div>
+
+
+
+			{/* scorekeeper */}
+			<div className="scoreKeeper">
+				{/*  */}
+			</div>
+
+		</div>
+	}
+
+}
+
+
 //    reveal buttons to declare right, wrong, or didn't answer
 //  after any of three buttons pressed
 //    make call to edit score if right or wrong
 //    call to toggle prompt
+
+
+
+
+
+
+
+
+
