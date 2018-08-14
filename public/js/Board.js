568e5a13e8bd2d9b900bb5e618ad6425a4f2715c
Alexis <alexis.bohlander@gmail.com>
2018-08-14 11:44:54 -0600

    navbar

--- public/js/Board.js
+++ public/js/Board.js
@@ -62,7 +62,7 @@ class Board extends React.Component {
 
 		return <div className="container">
 			<table>
-				<tr>
+				<tr className="category_data">
 					{categoryData}
 				</tr>
 				{boardData}
