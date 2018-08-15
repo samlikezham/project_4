23a3eb3782149bd17ab3b838d45ef7e82ecd4c11
Alexis <alexis.bohlander@gmail.com>
2018-08-15 17:09:09 -0600

    added puts to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User
 	if(ENV['DATABASE_URL'])
 			 uri = URI.parse(ENV['DATABASE_URL'])
+			 puts "using env database"
+			 puts uri
 			 DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
 	 else
 			 DB = PG.connect(host: "localhost", port: 5432, dbname: 'simplerails')
+			 puts "not using env database"
 	end
 
 
