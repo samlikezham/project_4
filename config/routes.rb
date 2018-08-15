82296d84ac45e48322b19de5d8411cdb272d2167
Sam <schung015@gmail.com>
2018-08-15 14:25:30 -0700

    fixed db

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do 
-  namespace :api do 
-    namespace :v1 do 
-     resources :users, only: [:index, :create, :destroy, :update]
-      root to: 'site#home'
-    end 
-  end 
+  
+get '/users', to: 'users#index'
+get '/users/:id', to: 'users#show'
+post '/users', to: 'users#create'
+delete '/users/:id', to: 'users#delete'
+put '/users/:id', to: 'users#update'
+
+
+
+  # namespace :api do 
+  #   namespace :v1 do 
+  #    resources :users, only: [:index, :create, :destroy, :update]
+  #     root to: 'site#home'
+  #   end 
+  # end 
 end
